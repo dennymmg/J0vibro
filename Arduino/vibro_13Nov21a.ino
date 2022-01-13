948d2e0d0985afa4a0028f94b66dc88b2a33999f
Denny Melkay M G <37368234+dennymmg@users.noreply.github.com>
2022-01-13 18:41:49 +0530

    Update vibro_13Nov21a.ino

--- Arduino/vibro_13Nov21a.ino
+++ Arduino/vibro_13Nov21a.ino
@@ -1,17 +1,19 @@
-// Date: 6th November 2021
+// Date: January 2022
 
 // Protocol for vibration detection
 // Speaker always on
-// Piezo always on with amplitude V (such that m_bias is between 0 and 2.303), say V = 4.0
+// Piezo always on with amplitude V (such that m_bias is between 0 and 2.405), say V = 5.0
 
 // step1: Program sends a char 'I' (in phase) to the Arduino
 // step2: Arduino receives 'I' and sets pins 8,9 to HIGH; resets pins 10,11 to LOW; triggers camera
-// step3: N frames are taken -- bscans computed and averaged to X1
-// step4: High on Pin 10 causes the phase shift of 180
-// step5: the program discards the next frame from the camera
-// step6: 5 frames are taken -- bscans computed and averaged to X2
-// step7: displays X2 - X1
-// step8: go back to step1
+// step3: One in-phase frame taken, bscan computed
+// step4: Program sends a char 'O' (in phase) to the Arduino
+// step5: Arduino receives 'O' and resets pins 8,9 to LOW; sets pins 10,11 to HIGH; triggers camera
+// step6: One out-of-phase frame taken, bscan computed
+// step7: Steps 1 through 6 repeated N times-- N in-phase bscans averaged to X1 and 
+//                                              N out-of-phase bscans averaged to X2
+// step8: compute vibration amplitude
+// step9: go back to step1
 
 void setup() {
     // put your setup code here, to run once:
@@ -36,28 +38,19 @@ float t1, t2;
 void loop() {
   // put your main code here, to run repeatedly:
   t1 = millis();
+  
   while(Serial.available() <= 0)
   {
-    // time out of 10 seconds
+    // time out of 3 seconds
     t2 = millis();
-    if(t2-t1 >30) // 10 seconds
+    if(t2-t1 > 50) // 10000 milliseconds (timeout)
     {
-      // PORT B = B0 - pin8, B1 - pin9, B2 - pin 10, ..., B4 - pin12
-      if (ch1=='I') 
-      {
-        PORTB = B00000011; // pins 8 and 9 reset to 0, pins 10 and 11 set to 1 
-        digitalWrite(13, HIGH);
-      }
-      else 
-      {
-        PORTB = B00001100; // pins 8 and 9 set to 1, pins 10 and 11 reset to 0
-        digitalWrite(13, LOW);
-      }
-      // level 
+      // level trigger
       PORTD = PORTD | B10000000; // set the pin7 D7  (camera trigger)
       delayMicroseconds(200);
       PORTD = PORTD & B01111111;
-    }   
+      t1 = t2;
+     }   
   }
     
   ch1 = Serial.read();
@@ -65,17 +58,17 @@ void loop() {
   if (ch1 == 'I') // I - in phase with speaker (non-inverting op amp)
   {
     // PORT B = B0 - pin8, B1 - pin9, B2 - pin 10, ..., B4 - pin12
-    PORTB = B00000011; // pins 8 and 9 set to 1, pins 10 and 11 reset to 0
+    PORTB = B00000011; // pins 8,9 set to 1 (switches S3&S4 ON), pins 10,11 reset to 0 (S1&S2 OFF) 
     PORTD = PORTD | B10000000; // set the pin7 D7  (camera trigger)
     delayMicroseconds(200);
     PORTD = PORTD & B01111111;
     digitalWrite(13, HIGH);
   }
   
-  if (ch1 == 'O')  // I - out of phase with speaker (inverting op amp)
+  if (ch1 == 'O')  // O - out of phase with speaker (inverting op amp)
   {
     // PORT B = B0 - pin8, B1 - pin9, B2 - pin 10, ..., B4 - pin12
-    PORTB = B00001100; // pins 8 and 9 reset to 0, pins 10 and 11 set to 1 
+    PORTB = B00001100; // pins 8,9 reset to 0 (switches S3&S4 OFF) , pins 10,11 set to 1 (S1&S2 ON)  
     PORTD = PORTD | B10000000; // set the pin7 D7  (camera trigger)
     delayMicroseconds(200);
     PORTD = PORTD & B01111111;
